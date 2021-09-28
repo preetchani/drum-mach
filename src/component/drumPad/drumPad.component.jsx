884be4990695c827b041cd53f7ab20fb095212f3
preetchani <preetchani23498@gmail.com>
2021-09-28 20:49:23 +0530

    first commit

---
+++ src/component/drumPad/drumPad.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class DrumPad extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            padstyle:inActiveS
+        }
+    }
+
+    render(){
+        return(
+           <div className="drum-pad"
+            
+           >
+
+           </div> 
+        );
+    }
+}
